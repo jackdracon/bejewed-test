6809a649597b9cb47e54d15d97bb35781c7752ee
Vinicius Bruno <vinicius@hiperverso.com>
2020-10-12 18:37:05 -0300

    Line Combinations
    - Line combinations between the same objects type
    - Delete objects with a succesful moviment
    - Create another object in parent with a delayd time
    - Check for adjascents objects

--- BejewedTest/Assets/Scripts/Objects/BaseObject.cs
+++ BejewedTest/Assets/Scripts/Objects/BaseObject.cs
@@ -8,17 +8,36 @@ using UnityEngine;
 /// </summary>
 public abstract class BaseObject : MonoBehaviour
 {
+    #region VARIABLES
+    [Tooltip("Object's type"), SerializeField]
+    private OBJECTTYPE type = OBJECTTYPE.DIAMOND;
+
+    //Map Reference on the map
     public Transform mapPositionReference;
+    #endregion
 
     #region METHODS
     /// <summary>
     /// SetMapReference is a way to guarantee a link to a tile on the map
     /// </summary>
     /// <param name="_ref"></param>
-    public virtual void SetMapReference(Transform _ref) 
+    public virtual void SetMapReference(Transform _ref)  { }
+
+    //Object's type
+    public OBJECTTYPE MyObjectType
     {
-        
+        get { return type; }
     }
 
     #endregion
+}
+
+//Object type
+public enum OBJECTTYPE 
+{ 
+    QUAD = 0,
+    TRIANGLE = 1,
+    DIAMOND = 2,
+    ELLIPSE = 3,
+    HEX = 4
 }
